beaef1de42f3b00b0f3050b733f54123ee695d4a
Marcus-Moen <109671218+Marcus-Moen@users.noreply.github.com>
2024-10-30 17:55:01 +0200

    This is the initial form setup. I added all the components for the front-end of the form. I also renamed each component respectively.
    I also created a folder for each of the 3 tiers.
    
    Addresses Issue #1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvDetails = new System.Windows.Forms.DataGridView();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblSearch = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDetails
+            // 
+            this.dgvDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetails.Location = new System.Drawing.Point(236, 12);
+            this.dgvDetails.Name = "dgvDetails";
+            this.dgvDetails.Size = new System.Drawing.Size(498, 307);
+            this.dgvDetails.TabIndex = 0;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(35, 58);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(165, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(32, 109);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name: ";
+            this.lblName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(35, 299);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(165, 20);
+            this.txtCourse.TabIndex = 3;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(35, 219);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(165, 20);
+            this.txtAge.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(35, 140);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(165, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(32, 29);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(61, 13);
+            this.lblID.TabIndex = 6;
+            this.lblID.Text = "Student ID:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(32, 192);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(29, 13);
+            this.lblAge.TabIndex = 7;
+            this.lblAge.Text = "Age:";
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Location = new System.Drawing.Point(32, 275);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(43, 13);
+            this.lblCourse.TabIndex = 8;
+            this.lblCourse.Text = "Course:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.LightBlue;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnAdd.Location = new System.Drawing.Point(35, 354);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(85, 32);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(136, 354);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(83, 32);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(418, 366);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(160, 20);
+            this.txtSearch.TabIndex = 11;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(457, 404);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(83, 32);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(475, 342);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(47, 13);
+            this.lblSearch.TabIndex = 13;
+            this.lblSearch.Text = "Search: ";
+            this.lblSearch.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(746, 471);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblCourse);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.dgvDetails);
+            this.Name = "Form1";
+            this.Text = "StudentManagementSystem";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetails)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvDetails;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblSearch;
     }
 }
 
