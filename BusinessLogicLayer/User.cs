dd65e5bba78a86a166d9f05ff4fbe26893d2dff5
Gito Gerardo Martin <600966@student.belgiumcampus.ac.za>
2024-11-04 21:43:16 +0200

    added functionality to the login

---
+++ BusinessLogicLayer/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem.BusinessLogicLayer
+{
+    internal class User
+    {
+        string Username;
+        string Password;
+        public User(string user,string pass)
+        {
+            this.Username = user;
+            this.Password = pass;
+        }
+        public string Users
+        {
+            get { return this.Username; }
+            set { this.Username = value; }
+        }
+        public string Pass
+        {
+            get { return this.Password; }
+            set { this.Password = value; }
+        }
+    }
+}
